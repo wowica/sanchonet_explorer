baa59757aaa05a4db6a310d143a5a54a9952cd5c
Carlos Souza <carloshrsouza@gmail.com>
2023-12-05 17:46:25 -0500

    Disable migrations

--- lib/sanchonet_explorer/release.ex
+++ lib/sanchonet_explorer/release.ex
@@ -3,26 +3,28 @@ defmodule SanchonetExplorer.Release do
   Used for executing DB release tasks when run in production without Mix
   installed.
   """
-  @app :sanchonet_explorer
+
+  # @app :sanchonet_explorer
 
   def migrate do
-    load_app()
+    raise "Should never run migrate"
+    # load_app()
 
-    for repo <- repos() do
-      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
-    end
+    # for repo <- repos() do
+    #   {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+    # end
   end
 
-  def rollback(repo, version) do
-    load_app()
-    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
-  end
+  # def rollback(repo, version) do
+  #   load_app()
+  #   {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  # end
 
-  defp repos do
-    Application.fetch_env!(@app, :ecto_repos)
-  end
+  # defp repos do
+  #   Application.fetch_env!(@app, :ecto_repos)
+  # end
 
-  defp load_app do
-    Application.load(@app)
-  end
+  # defp load_app do
+  #   Application.load(@app)
+  # end
 end
